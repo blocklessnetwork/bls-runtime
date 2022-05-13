acd2ae7632e7f7fff0e1364bb69313f408da275f
gongchengzhi <gongcz@gmail.com>
2022-05-13 14:51:09 +0800

    add stdout file support in the sandbox file system

--- blockless/src/config.rs
+++ blockless/src/config.rs
@@ -34,7 +34,8 @@ impl BlocklessConfig {
         }
     }
 
-    /// stdout file must be work in sandbox root_path, if root_path is not setting, the stdout file will use Inherit
+    /// stdout file must be work in sandbox root_path, 
+    /// if root_path is not setting, the stdout file will use Inherit
     pub fn stdout(&mut self, stdout: Stdout) {
         self.stdout = stdout
     }
