8c8e5add2076ee624f1631bdcbbb0ce0c6c70cb4
gongchengzhi <gongcz@koal.com>
2023-01-07 10:00:23 +0800

    add the db mod

--- crates/blockless-drivers/src/cgi_driver/db.rs
+++ crates/blockless-drivers/src/cgi_driver/db.rs
@@ -1,17 +1,43 @@
-use std::sync::Once;
+use rusqlite::Connection;
+use anyhow::Result;
 
-use sqlite::{self, Connection};
+pub(crate) struct ExtensionMeta {
+    pub md5: String,
+    pub description: String,
+    pub alias: String,
+}
 
-fn get_connect(file: &str) -> Option<&mut Connection> {
-    static mut CONNECT: Option<Connection> = None;
-    static CONNECT_ONCE: Once = Once::new();
-    CONNECT_ONCE.call_once(|| {
-        unsafe {
-            CONNECT = sqlite::open(file).ok();
-        }
-    });
-    unsafe {
-        CONNECT.as_mut()
+struct DB {
+    connect: Connection
+}
+
+impl DB {
+    pub(crate) fn new(file: &str) -> Result<DB> {
+        let connect = Connection::open(file)?;
+        Ok(Self{
+            connect
+        })
+    }
+
+    pub(crate) fn create_schema() -> Result<()> {
+
+        Ok(())
+    }
+
+    pub(crate) fn save_extension_meta(
+        self: &mut DB, 
+        meta: &ExtensionMeta
+    ) -> Result<()> {
+        let insert_sql = r#"
+            insert into 
+            extension_meta(alias, md5, description)
+            values(?1,?2,?3);
+        "#;
+        self.connect.execute(
+            insert_sql, 
+            (&meta.alias, &meta.md5, &meta.description)
+        )?;
+        Ok(())
     }
 }
 
