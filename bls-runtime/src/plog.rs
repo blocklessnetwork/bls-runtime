d88445c32dfd4562f0f56ca4210b7b7506c67297
Join.G <joinhack@gmail.com>
2024-04-04 08:49:47 +0800

    improve: use the log replace the error

--- bls-runtime/src/plog.rs
+++ bls-runtime/src/plog.rs
@@ -1,19 +1,25 @@
 use std::{fmt::Arguments, sync::Once};
 
 use env_logger::Logger;
-use log::{Log, Level, Record};
+use log::{Level, Log, MetadataBuilder, Record};
 
 static mut ENV_LOGGER: Option<Logger> = None;
 
 /// The logger should only be set once in log crate by set_logger,
 /// if set the console as output, can't be set the file as output.
-/// Therefore, we need a logger where the console is set as output 
+/// Therefore, we need a logger where the console is set as output
 /// before initializing the env_log by the configure file.
 pub(crate) fn env_logger() -> Option<&'static mut Logger> {
     static ONCE: Once = Once::new();
     unsafe {
         ONCE.call_once(|| {
-            let logger = Logger::from_default_env();
+            // the log metadata
+            let metadata = MetadataBuilder::new().build();
+            let mut builder = env_logger::Builder::from_default_env();
+            // set the console as output.
+            builder.target(Default::default());
+            let logger = builder.build();
+            logger.enabled(&metadata);
             ENV_LOGGER = Some(logger);
         });
         ENV_LOGGER.as_mut()
@@ -22,11 +28,8 @@ pub(crate) fn env_logger() -> Option<&'static mut Logger> {
 
 /// log info by level
 pub fn plog(level: Level, args: Arguments<'_>) {
-    let record = Record::builder()
-        .args(args)
-        .level(level)
-        .build();
-    env_logger().unwrap().log(&record);
+    let record = Record::builder().args(args).level(level).build();
+    env_logger().map(|l| l.log(&record));
 }
 
 #[cfg(test)]
